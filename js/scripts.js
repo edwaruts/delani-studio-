b70b66fb6aeca4aef31e15f500d11239ea53ab2e
edwaruts <edwarutere@gmail.com>
2019-05-09 14:11:42 +0300

    fixed margin issue

--- js/scripts.js
+++ js/scripts.js
@@ -14,134 +14,175 @@ $(document).ready(function() {
     });
 });
 $(document).ready(function() {
-    $('.text').hide();
-    $('#div1').animate({
+    $(".text").hide();
+    $("#div1").animate({
         opacity: 1
-
     });
 
-    $('#div1').hover(function() {
-        $(this).stop().animate({ opacity: .4 }, 200);
-        $('.text').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({ opacity: 1 }, 500)
-        $('.text').fadeOut();
-    });
+    $("#div1").hover(
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 0.4 }, 200);
+            $(".text").fadeIn();
+        },
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 1 }, 500);
+            $(".text").fadeOut();
+        }
+    );
 });
 $(document).ready(function() {
-    $('.text2').hide();
-    $('#div2').animate({
+    $(".text2").hide();
+    $("#div2").animate({
         opacity: 1
-
     });
 
-    $('#div2').hover(function() {
-        $(this).stop().animate({ opacity: .4 }, 200);
-        $('.text2').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({ opacity: 1 }, 500)
-        $('.text2').fadeOut();
-    });
+    $("#div2").hover(
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 0.4 }, 200);
+            $(".text2").fadeIn();
+        },
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 1 }, 500);
+            $(".text2").fadeOut();
+        }
+    );
 });
 $(document).ready(function() {
-    $('.text3').hide();
-    $('#div3').animate({
+    $(".text3").hide();
+    $("#div3").animate({
         opacity: 1
-
     });
 
-    $('#div3').hover(function() {
-        $(this).stop().animate({ opacity: .4 }, 200);
-        $('.text3').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({ opacity: 1 }, 500)
-        $('.text3').fadeOut();
-    });
+    $("#div3").hover(
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 0.4 }, 200);
+            $(".text3").fadeIn();
+        },
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 1 }, 500);
+            $(".text3").fadeOut();
+        }
+    );
 });
 $(document).ready(function() {
-    $('.text4').hide();
-    $('#div4').animate({
+    $(".text4").hide();
+    $("#div4").animate({
         opacity: 1
-
     });
 
-    $('#div4').hover(function() {
-        $(this).stop().animate({ opacity: .4 }, 200);
-        $('.text4').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({ opacity: 1 }, 500)
-        $('.text4').fadeOut();
-    });
+    $("#div4").hover(
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 0.4 }, 200);
+            $(".text4").fadeIn();
+        },
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 1 }, 500);
+            $(".text4").fadeOut();
+        }
+    );
 });
 $(document).ready(function() {
-    $('.text5').hide();
-    $('#div5').animate({
+    $(".text5").hide();
+    $("#div5").animate({
         opacity: 1
-
     });
 
-    $('#div5').hover(function() {
-        $(this).stop().animate({ opacity: .4 }, 200);
-        $('.text5').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({ opacity: 1 }, 500)
-        $('.text').fadeOut();
-    });
+    $("#div5").hover(
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 0.4 }, 200);
+            $(".text5").fadeIn();
+        },
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 1 }, 500);
+            $(".text").fadeOut();
+        }
+    );
 });
 $(document).ready(function() {
-    $('.text6').hover();
-    $('#div6').animate({
+    $(".text6").hover();
+    $("#div6").animate({
         opacity: 1
-
     });
 
-    $('#div6').hover(function() {
-        $(this).stop().animate({ opacity: .4 }, 200);
-        $('.text6').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({ opacity: 1 }, 500)
-        $('.text6').fadeOut();
-    });
+    $("#div6").hover(
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 0.4 }, 200);
+            $(".text6").fadeIn();
+        },
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 1 }, 500);
+            $(".text6").fadeOut();
+        }
+    );
 });
 $(document).ready(function() {
-    $('.text7').hide();
-    $('#div7').animate({
+    $(".text7").hide();
+    $("#div7").animate({
         opacity: 1
-
     });
 
-    $('#div7').hover(function() {
-        $(this).stop().animate({ opacity: .4 }, 200);
-        $('.text7').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({ opacity: 1 }, 500)
-        $('.text7').fadeOut();
-    });
+    $("#div7").hover(
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 0.4 }, 200);
+            $(".text7").fadeIn();
+        },
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 1 }, 500);
+            $(".text7").fadeOut();
+        }
+    );
 });
 $(document).ready(function() {
-    $('.text8').hide();
-    $('#div8').animate({
+    $(".text8").hide();
+    $("#div8").animate({
         opacity: 1
-
     });
 
-    $('#div8').hover(function() {
-        $(this).stop().animate({ opacity: .4 }, 200);
-        $('.text8').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({ opacity: 1 }, 500)
-        $('.text8').fadeOut();
-    });
+    $("#div8").hover(
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 1 }, 300);
+            $(".text8").fadeIn();
+        },
+        function() {
+            $(this)
+                .stop()
+                .animate({ opacity: 1 }, 700);
+            $(".text8").fadeOut();
+        }
+    );
 });
 
 function myFunction() {
-    document.getElementById("demo").innerHTML = "we have received your message thanks for reaching us.";
+    document.getElementById("demo").innerHTML =
+        "we have received your message,we shall get back to you shortly thank you";
 }
